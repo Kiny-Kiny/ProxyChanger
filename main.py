3a029fec850984ff0a7e3ba0ccddbb79bbfdde72
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-11-14 18:13:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
-#---------------------------------------#
+from threading import Thread;
+from random import choice;
+from subprocess import call;
+from sys import executable;
+from time import sleep;
+from os import name;
+
+ip_local=['192.168.0.1',  '192.168.0.2', '192.168.0.3','192.168.0.10'];
+
+with open('proxies.txt','r') as f:
+	proxies=str(f.read())
+	proxies=proxies.split('\n')
+
+call([executable,'-m','pip','install','requests']);
+from requests import get;
+
 global R,B,C,G
-R='\033[1;31m';
-B='\033[1;34m';
-C='\033[1;37m';
-G='\033[1;32m';
-Format="\033[0m";
-Letra="\033[38;5;15m";
-Fundo="\033[48;5;19m"
-banner='''
- __  __     __     __   __     __  __    
-/\ \/ /    /\ \   /\ "-.\ \   /\ \_\ \   
-\ \  _"-.  \ \ \  \ \ \-.  \  \ \____ \  
- \ \_\ \_\  \ \_\  \ \_\\"\_\  \/\_____\ 
-  \/_/\/_/   \/_/   \/_/ \/_/   \/_____/ \n
-'''
-#---------------------------------------#
-from os import system
-from sys import argv
-from os import execl
-from sys import executable
-#---------------------------------------#
-def clear():
-	system('cls');system('clear')
-#---------------------------------------#
-try:
-	from requests import get
-except:
-	system('pip install requests');print('_ ! _ Instalação do módulo requests foi concluída. Execute o script novamente. _ ! _');exit()
-try:
-	clear();time=int(input(f'''{B}{banner}_ ! _ {Fundo}{Letra}{C}Digite os segundos que deseja ficar conectado{Format} {B}>>>{C} '''))
-except:
-	print(f'{R}_ ! _{C} Caracteres inválidos. {R}_ ! _{C}');exit()
-#---------------------------------------#
-while(time > 0):
-	try:
-		from time import sleep
-		r=get('http://pubproxy.com/api/proxy').json()
-		proxy=r['data'][0]['ip']+':'+r['data'][0]['port']
-		response=get('https://httpbin.org/ip', proxies={'http': proxy, 'https': proxy}).json()
-		try:
-			print(f'{G}Conectado a proxy: '+response['origin'])
-		except:
-			print(f'{R}Não foi possível se conectar a proxy: {proxy}')
-		#print(response)
-		sleep(1)
-		time=time-1
-	except:
-		pass
-#---------------------------------------#
-op=input(f'{B}_ ! _{C} Deseja continuar?{B}_ ! _{C} \n{B}1{C} - Sim\n{B}2{C} - Não\n{B}>>>{C} ')
-if op == '1': execl(executable, executable, *argv)
-elif op == '2': system('cls');system('clear');exit()
-else: print(f'{R}_ ! _{C} Opção inválida. {R}_ ! _{C}')
-#---------------------------------------#
+Vermelho='\033[1;31m';Azul='\033[1;34m';Branco='\033[1;37m';Verde='\033[1;32m';
+
+call([{'nt':'cls','posix':'clear'}[name]])
+
+print('''%s  __  __     __     __   __     __  __    
+ /\ \/ /    /\ \   /\ "-.\ \   /\ \_\ \   
+ \ \  _"-.  \ \ \  \ \ \-.  \  \ \____ \  
+  \ \_\ \_\  \ \_\  \ \_\\"\_\   \/\_____\ 
+   \/_/\/_/   \/_/   \/_/ \/_/   \/_____/ \n%s'''%(Azul,Branco))
+
+def main(proxy):
+	call(['ifconfig','eth0','down']);
+	call(['ifconfig','eth0',choice(ip_local),'up']);
+	try: print('[ %s!%s ] Conectado a %s%s%s.'%(Verde,Branco,Verde,get('https://httpbin.org/ip',proxies={'http://':proxy,'https://':proxy}).json()['origin'],Branco))
+	except Exception: print('[ %sX%s ] Não foi possível se conectar a %s%s%s.'%(Vermelho,Branco,Vermelho,proxy,Branco))
+	
+if __name__ == '__main__':
+	while True:
+		for ip in proxies: Thread(target=main(ip)).start();sleep(0.7)
